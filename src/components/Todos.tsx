21050c350f698c23b08022d001278dd92e6bdaef
vardnan <vardnansivarajah@gmail.com>
2024-03-02 14:47:41 -0500

    add more components and build out functionality for todolist

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import TodoItem from './TodoItem';
+import Todo from '../models/todo';
 
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <TodoItem key={item.id} text={item.text}/>
       ))}
     </ul>
   );
-};
+}; 
 
 export default Todos;
