b8614dde9ec8d8e87056749861ea0e7a80e88565
vardnan <vardnansivarajah@gmail.com>
2024-03-03 20:32:32 -0500

    implement funcitonality with context api and typescript

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import TodoItem from './TodoItem';
-import Todo from '../models/todo';
+import { TodosContext } from '../store/todos-context';
 import classes from './Todos.module.css';
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text}/>
+      {todosCtx.items.map((item) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
-}; 
+};
 
 export default Todos;
